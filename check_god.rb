60b41a18e755de25d4c8532e65636acb74bcc6df
Alex Dean <alex@crackpot.org>
2011-12-13 08:35:19 -0600

    add check script for god. http://god.rubyforge.org/. critical if any service is not 'up'.

---
+++ check_god.rb
@@ -0,0 +1,59 @@
+require 'optparse'
+
+OK = 0
+WARNING = 1
+CRITICAL = 2
+
+def exit_with(status, message)
+  puts message
+  exit status
+end
+
+options = {}
+optparse = OptionParser.new do|opts|
+  opts.banner = "Usage: check_god.rb -v -V -h"
+
+  options[:verbose] = false
+  opts.on( '-v', '--verbose', 'Output more information' ) do
+    options[:verbose] = true
+  end
+
+  opts.on( '-V', '--version', 'Output version information' ) do
+    puts "Nagios god monitor version 1.0"
+    exit OK
+  end
+
+  opts.on( '-h', '--help', 'Display this screen' ) do
+    puts opts
+    exit OK
+  end
+end
+
+begin
+  optparse.parse!
+rescue OptionParser::InvalidOption => e
+  exit_with( CRITICAL, e )
+end
+
+god_status = `sudo god status`
+if $? != 0
+  exit_with( CRITICAL, god_status )
+end
+
+puts "'god status' says:\n#{god_status}" if options[ :verbose ]
+
+# split on newlines
+items = god_status.split( "\n" ).map { |l|
+  # split each line on spaces and remove leading/trailing whitespace
+  l.split( ': ' ).map { |p| p.strip }
+}
+# reject group items, which have no status.
+items.reject!{ |i| i.size != 2 }
+
+status = OK
+# critical if any watch is not 'up'
+if items.select{ |i| i[ 1 ] != 'up' }.size > 0
+  status = CRITICAL
+end
+
+exit_with( status, items.map{ |i| i.join( ':' ) }.join( ', ' ) )
